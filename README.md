a293d5922e0f4dee3b6ff1ee617ab2f5f44225a4
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2024-10-23 18:33:22 +0530

    project: Upload Area and Circumference of Circle Program

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ This repository contains PPC18 Lab Programs for 1st Year Computer Science lab se
 | Program No. | Title/Description                                              | File Link                | Output Link                        |
 | ------------| ------------------------------------------------------------- | ------------------------ | ----------------------------------- |
 | 1           | C Program to find sum of three numbers                       | [sum.c](./sum.c)       | [Program 1](Outputs/sum.txt)   |
+| 2           | C Program to find area and circumference of a circle         | [area_circle.c](./area_circle.c) | [Program 2](Outputs/area_circle.txt) |
+
 ## License
 
 This project is licensed under the MIT License. See the [LICENSE](./LICENSE) file for details.
---
+++ area_circle.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/* The Above Line imports the stdio.h into our program for supporting input and output functions such as scanf() and printf()
+without this scanf() and prinf() simply wont work 
+*/
+
+#define PI 3.14159 
+/* The Above Line Defines a constant value for the variable PI which cannot be changed later in the code. */
+
+int main() {
+    float r, area, cmf; 
+	/* The Above Line Declares variables for the radius, area, and circumference of the circle as r,area,cmf respectively. */
+
+    // Code for Taking input for the radius of the circle
+    printf("Enter the radius of the circle: ");
+    scanf("%f", &r); 
+	/* The Above Line Reads the radius from user input. */
+
+    // Calculating the area of the circle using the formula: Area = π * r^2
+    area = PI * r * r; /* The Above Line Calculates the area using the defined formula. */
+
+    // Calculating the circumference of the circle using the formula: Circumference = 2 * π * r
+    cmf = 2 * PI * r; /* The Above Line Calculates the circumference using the defined formula. */
+
+    // Output the results of area and circumference calculations
+    printf("The area of the circle is: %.2f\n",area); /* Output the area result. */
+    printf("The circumference of the circle  is: %.2f\n",cmf); /* Output the circumference result. */
+
+    return 0; /* The Above Line Indicates that the program has finished successfully. */
+}
